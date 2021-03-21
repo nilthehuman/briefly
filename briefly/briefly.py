68cb9b0d6e80cf77eaab33f95f8d08ebc5afe50a
aratod <nil.the.human@gmail.com>
2021-03-21 19:22:34 +0100

    Move chief entry point to briefly/

--- briefly/briefly.py
+++ briefly/briefly.py
@@ -5,13 +5,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def nothing():
+    return "Nothing to see here, move along."
+
+@app.route('/echo/<input>')
+def echo(input):
+    return input
+
+@app.route('/briefly/')
+@app.route('/briefly/<input>')
 def index(url):
     """The main entry point.
     Takes an HTTP URL as parameter to fetch and analyze.
     """
     return render_template('index.html')
-
-@app.route('/echo/<input>')
-def echo(input):
-    return input
 
