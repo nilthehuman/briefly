0b7016e6c1bef32e743e49425d447403ad2cd8f5
aratod <nil.the.human@gmail.com>
2021-05-09 19:55:17 +0200

    Looks like you need the hex code of the emoji

--- briefly/briefly.py
+++ briefly/briefly.py
@@ -27,6 +27,6 @@ def index(url=None):
         if is_in_english(response):
             response = strip_common_words(response)
         else:
-            response = 'That webpage does not seem to be written in english. &#129320;'
+            response = 'That webpage does not seem to be written in english. &#x1F928;'
     return render_template('index.html', url=url, response=response)
 
