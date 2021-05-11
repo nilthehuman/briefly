aa8898cae328729c25ccb9677f7b92d69003b400
aratod <nil.the.human@gmail.com>
2021-05-11 22:46:17 +0200

    Prefer keywords that turn up in more topics

--- briefly/score.py
+++ briefly/score.py
@@ -1,6 +1,7 @@
 """The core topic identification model. Contains functions to calculate the relative score
 of each distinct uncommon word in the English text input to help identify the topic."""
 
+from collections import Counter
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import wordnet
 from gensim.corpora import Dictionary
@@ -23,7 +24,8 @@ def gensim_lda(string):
     # [(0, '0.148*"cats"'), (1, '0.225*"mice"'), (2, '0.150*"dogs"')]
     topic_vector = lda_model.print_topics(num_words=2)
     topics = [t.split('"')[1] for (_, t) in topic_vector] + [t.split('"')[3] for (_, t) in topic_vector]
-    return list(set(topics))
+    best_topics = list(t for t,_ in Counter(topics).most_common()[0:3])
+    return best_topics
 
 def bag_of_words(string):
     """Count the number of times each distinct word occurs.
